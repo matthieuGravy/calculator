63f6ee02ab9f11a4021b46bbc364b1132f377665
matthieuGravy <matthieugravy@gmail.com>
2023-10-24 14:59:03 +0200

    base

---
+++ JS/calculator.js
@@ -0,0 +1,16 @@
+function somme(a, b) {
+  return a + b;
+}
+
+function soustraction(a, b) {
+  return a / b;
+}
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+function divise(a, b) {
+  return a / b;
+}
+console.log(divise(1, 3));
