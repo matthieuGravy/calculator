a9e149b22734a0878631ff6d2e92e0963beee3a1
matthieuGravy <matthieugravy@gmail.com>
2023-10-24 15:43:45 +0200

    correction touches

--- JS/touche.js
+++ JS/touche.js
@@ -3,20 +3,19 @@ function detection(a, event) {
     console.log(a);
   }
 }
-const number = 0;
-document.addEventListener("keydown", function (event) {
-  detection(number.toString(), event);
-});
+for (let i = 0; i <= 9; i++) {
+  document.addEventListener("keydown", function (event) {
+    detection(i.toString(), event);
+  });
+}
 
 const elements = [];
-
 for (let i = 0; i <= 9; i++) {
   const element = document.getElementById(`number-${i}`);
   if (element) {
     elements.push(element);
   }
 }
-
 elements.forEach(function (element, index) {
   element.addEventListener("click", () => {
     console.log(`click ${index}`);
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 import "./style.css";
 import "./JS/calculator";
 import "./JS/touche";
+import "./JS/screen";
