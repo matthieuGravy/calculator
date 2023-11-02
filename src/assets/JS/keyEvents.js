dc91a072107190adfaae985e39f09f6708c9822f
matthieuGravy <matthieugravy@gmail.com>
2023-11-02 21:41:04 +0100

    entries

--- src/assets/JS/keyEvents.js
+++ src/assets/JS/keyEvents.js
@@ -12,7 +12,7 @@ function detection(a, event) {
 }
 
 function toDom(term, a) {
-  term.textContent = a.join("");
+  term.textContent = a.join("").replace(/,/g, "");
 }
 
 /**
@@ -31,16 +31,16 @@ for (let i = 0; i <= 9; i++) {
  * Adding click event listeners to number elements
  */
 let a = [];
+let b = [];
 let operatorUsed = false;
 
 elements.forEach((element, index) => {
   element.addEventListener("click", () => {
     console.log(`click ${index}`);
     if (operatorUsed) {
-      a = [];
-      a.push(index);
-      console.log(a);
-      toDom(termTwo, a);
+      b.push(index);
+      console.log(b);
+      toDom(termTwo, b);
     } else {
       a.push(index);
       console.log(a);
@@ -68,6 +68,7 @@ operators.forEach((operator) => {
     });
   }
 });
+
 /**
  * Event listener for keydown events related to operators
  */
@@ -75,5 +76,18 @@ document.addEventListener("keydown", (event) => {
   const key = event.key;
   if (operators.includes(key)) {
     console.log(key);
+    operatorUsed = true;
+    areaOperator.textContent = key;
+  }
+  if (!isNaN(key) && key >= 0 && key <= 9) {
+    if (operatorUsed) {
+      b.push(parseInt(key));
+      console.log(b);
+      toDom(termTwo, b);
+    } else {
+      a.push(parseInt(key));
+      console.log(a);
+      toDom(termOne, a);
+    }
   }
 });
